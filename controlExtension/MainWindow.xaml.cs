8bb8f642ebb73a47a2920275df0cbcd31ee08a75
Губанов Александр <alexgubanow@outlook.com>
2018-05-13 14:08:27 +0300

    cleanUP

--- controlExtension/MainWindow.xaml.cs
+++ controlExtension/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace controlExtension
         private SerialPort mySerialPort;
         private DispatcherTimer dispatcherTimer;
         private Thread runExprThrd;
-        private Thread UpdZedThrd;
         private int endstopsState;
 
         public MainWindow()
@@ -32,19 +31,11 @@ namespace controlExtension
             Vm = new MainViewModel();
             InitializeComponent();
             DataContext = Vm;
-
+            initComPort(1);
             dispatcherTimer = new DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 100);
             dispatcherTimer.Start();
-            mySerialPort = new SerialPort("COM1");
-            mySerialPort.BaudRate = 115200;
-            mySerialPort.Parity = Parity.None;
-            mySerialPort.StopBits = StopBits.One;
-            mySerialPort.DataBits = 8;
-            mySerialPort.Handshake = Handshake.None;
-            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-            Vm.comPort.avaibleComPorts = SerialPort.GetPortNames();
         }
 
         private void ApplyEffect(Window win)
@@ -53,7 +44,6 @@ namespace controlExtension
             {
                 System.Windows.Media.Effects.BlurEffect objBlur = new System.Windows.Media.Effects.BlurEffect();
                 objBlur.Radius = 4;
-                //mngr.Visibility = Visibility.Collapsed;
                 overlayrect.Visibility = Visibility.Visible;
                 overlayring.Visibility = Visibility.Visible;
                 mainPanel.Effect = objBlur;
@@ -67,54 +57,12 @@ namespace controlExtension
             {
                 mainPanel.IsEnabled = true;
                 mainPanel.Effect = null;
-                //mngr.Visibility = Visibility.Visible;
                 overlayrect.Visibility = Visibility.Collapsed;
                 overlayring.Visibility = Visibility.Collapsed;
             }));
         }
 
-        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
-        {
-            if (mySerialPort.IsOpen)
-            {
-                string str = mySerialPort.ReadExisting();
-                Vm.comPort.answer = str;
-                try
-                {
-                    JObject inJSON = JObject.Parse(str);
-                    switch (inJSON["Comm"].ToString())
-                    {
-                        case "STAT?":
-                            Vm.MainWin.Status = bool.Parse(inJSON["status"].ToString()) ? "Good" : "Bad";
-                            Vm.MainWin.Status = "Receive status command \"STAT?\" = " + Vm.MainWin.Status;
-                            break;
-                        case "hx711?":
-                            Vm.exper.time.Add(DateTime.UtcNow - Vm.exper.tStart);
-                            Vm.hx711.Value = inJSON["val"].ToString();
-                            Vm.MainWin.Status = "Receive hx711 value command \"hx711?\" = " + Vm.hx711.Value;
-                            Vm.exper.rawData.Add(Convert.ToDouble(Vm.hx711.Value));
-                            Vm.exper.miu.Add(Convert.ToDouble(Vm.hx711.Value) / Vm.exper.massa);
-                            Dispatcher.Invoke(new Action(() =>
-                            {
-                                RAWdataPlot.graphPane.CurveList[0].AddPoint(Vm.exper.time[Vm.exper.time.Count - 1].TotalMilliseconds, Vm.exper.rawData[Vm.exper.rawData.Count - 1]);
-                                miuPlot.graphPane.CurveList[0].AddPoint(Vm.exper.time[Vm.exper.time.Count - 1].TotalMilliseconds, Vm.exper.miu[Vm.exper.miu.Count - 1]);
-                                updZed(RAWdataPlot.ZedGraphPlot);
-                                updZed(miuPlot.ZedGraphPlot);
-                            }));
-                            break;
-
-                        case "endStops?":
-                            endstopsState = Convert.ToInt32(inJSON["val"]);
-                            Vm.MainWin.Status = "Receive endstops State command \"endStops?\" = " + endstopsState;
-                            break;
-                    }
-                }
-                catch
-                {
-                    Vm.MainWin.Status = "Error parsing json";
-                }
-            }
-        }
+        
 
         private void updZed(ZedGraphControl zedGraph)
         {
@@ -134,8 +82,7 @@ namespace controlExtension
         {
             if (mySerialPort.IsOpen)
             {
-                mySerialPort.WriteLine("{\"Comm\":\"hx711?\"}");
-                Vm.MainWin.Status = "Sending command \"hx711?\"";
+                sendComm("hx711?");
             }
         }
 
@@ -157,8 +104,7 @@ namespace controlExtension
 
         private void btnData_Click(object sender, RoutedEventArgs e)
         {
-            mySerialPort.WriteLine("{\"Comm\":\"hx711?\"}");
-            Vm.MainWin.Status = "Sending command \"hx711?\"";
+            sendComm("hx711?");
         }
 
         private void autoGetCheckBox_Checked(object sender, RoutedEventArgs e)
@@ -191,16 +137,6 @@ namespace controlExtension
             }
         }
 
-        private void initComPort(int comPortNum)
-        {
-            mySerialPort = new SerialPort(Vm.comPort.avaibleComPorts[comPortNum]);
-            mySerialPort.BaudRate = 115200;
-            mySerialPort.Parity = Parity.None;
-            mySerialPort.StopBits = StopBits.One;
-            mySerialPort.DataBits = 8;
-            mySerialPort.Handshake = Handshake.None;
-            mySerialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
-        }
 
         private void comPorts_ComboBox_DropDownOpened(object sender, EventArgs e)
         {
@@ -306,31 +242,8 @@ namespace controlExtension
                 while (endstopsState != 1 && mySerialPort.IsOpen)
                 {
                     sendComm("endStops?");
-                    //Thread.Sleep(100);
                     sendComm("hx711?");
                 }
-            }
-        }
-        private void sendComm(string Comm)
-        {
-            if(mySerialPort.IsOpen)
-            {
-                mySerialPort.WriteLine("{\"Comm\":\"" + Comm + "\"" + "}");
-                Vm.MainWin.Status = "Sending" + "{\"Comm\":\"" + Comm + "\"" + "}";
-            }
-        }
-
-        private void sendComm(string Comm, string[] ParamNames, string[] Params)
-        {
-            if (mySerialPort.IsOpen)
-            {
-                string ParamBuf = "";
-                for (int i = 0; i < ParamNames.Length; i++)
-                {
-                    ParamBuf += ",\"" + ParamNames[i] + "\":" + Params[i];
-                }
-                mySerialPort.WriteLine("{\"Comm\":\"" + Comm + "\"" + ParamBuf + "}");
-                Vm.MainWin.Status = "Sending" + "{\"Comm\":\"" + Comm + "\"" + ParamBuf + "}";
             }
         }
         private void runExperBtn_Click(object sender, RoutedEventArgs e)
